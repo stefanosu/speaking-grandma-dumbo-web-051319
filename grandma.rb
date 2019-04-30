79134c6ebdd2e3f34814ee8739afbbcefcd9095e
Stefanos Ugbit <stefanosu422@gmail.com>
2019-04-30 21:57:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,16 +14,15 @@
 
 def speak_to_grandma(phrase)
  
- if phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
-
-    elsif phrase == phrase.downcase
+ def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+  
+  else
     return "HUH?! SPEAK UP, SONNY!"
     
-    elsif phrase == phrase.upcase
-      return "NO, NOT SINCE 1938!"
-
-    else
-  		return "NO, NOT SINCE 1938!"
-    end
   end
+end
